42800a5813324ce19699cac4ea6d2aa0b52e2f7b
Camilla <camilla.lima000@gmail.com>
2021-11-30 21:47:27 +0000

    Final solution with styling

--- app.rb
+++ app.rb
@@ -1,26 +1,32 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/game'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
-  configure :development do
-    register Sinatra::Reloader
-  end
-
   get '/' do
-   erb :index
+    erb :index
   end
-
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
+  end
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @game = $game
     erb :play
   end
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.opponent_of(@game.current_turn))
+    erb :attack
+  end
 
-  post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    redirect '/play'
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
   end
 
   # start the server if ruby file executed directly
